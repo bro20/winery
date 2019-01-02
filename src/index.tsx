df045f15f281f2a76f7695802d0d30c32e63033c
bro20 <veronica.brosio@gmail.com>
2019-01-02 19:27:00 +0100

    Add Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.scss";
-import Root from "./components/Root";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+//import Firebase, { FirebaseContext } from "./components/Firebase";
+import Root from "./components/Root";
+
+ReactDOM.render(
+	//<FirebaseContext.Provider value={new Firebase()}>
+	<Root />,
+	//</FirebaseContext.Provider>,
+	document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
